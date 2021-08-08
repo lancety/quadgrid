a2b80fe1f31546d1ac5b77df733aba05d7ebb77a
ltian <ltian@networkten.com.au>
2021-08-08 16:49:53 +1000

    use standard performance test case

--- src/demo.ts
+++ src/demo.ts
@@ -57,25 +57,25 @@ function _random(min, max) {
     const arr = Array(arrSize).fill(null);
     arr.forEach((ignore, r) => {
         arr.forEach((ignore, c) => {
-            // const w = _random(min, max) * (large ? 30 : 1) / 2;
-            // const h = _random(min, max) * (large ? 30 : 1) / 2;
-            const w = _random(min, max) * (large || (amount >= 10 && r < arrSize * 0.1) ? 20 : 1) / 2;
-            const h = _random(min, max) * (large || (amount >= 10 && c < arrSize * 0.1) ? 20 : 1) / 2;
-            states.rects.push([
-                _random(w, width - w),
-                _random(h, height - h),
-                w,
-                h,
-            ] as iBound)
-
-            // const w  =min * (large || (amount >= 10 && r < amount * 0.1) ? 30 : 1) / 2;
-            // const h = min * (large || (amount >= 10 && c < amount * 0.1) ? 30 : 1) / 2;
-            // states.rects.push( [
-            //     w * 3 * r * 1.1 + w,
-            //     h * 3 * c * 1.1 + h,
+            // // const w = _random(min, max) * (large ? 30 : 1) / 2;
+            // // const h = _random(min, max) * (large ? 30 : 1) / 2;
+            // const w = _random(min, max) * (large || (amount >= 10 && r < arrSize * 0.1) ? 20 : 1) / 2;
+            // const h = _random(min, max) * (large || (amount >= 10 && c < arrSize * 0.1) ? 20 : 1) / 2;
+            // states.rects.push([
+            //     _random(w, width - w),
+            //     _random(h, height - h),
             //     w,
             //     h,
             // ] as iBound)
+
+            const w  =min * (large || (amount >= 10 && r < amount * 0.1) ? 30 : 1) / 2;
+            const h = min * (large || (amount >= 10 && c < amount * 0.1) ? 30 : 1) / 2;
+            states.rects.push( [
+                w * 3 * r * 1.1 + w,
+                h * 3 * c * 1.1 + h,
+                w,
+                h,
+            ] as iBound)
         })
 
     })
